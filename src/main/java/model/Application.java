eebd4d8f9064307671d8c521de7a538987879267
Nandini-00123 <nandinib099@mail.com>
2023-09-07 16:29:15 +0530

    model classes created

---
+++ src/main/java/model/Application.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Project!");
+    }
+
+}
---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Customer {
+    int id;
+    String address;
+    long phone;
+    String city;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
